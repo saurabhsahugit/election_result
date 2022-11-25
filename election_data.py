1008056c2c5e8f31c31e481cbc1d5a61a2e5bad4
saurabhsahugit <saurabh.newhorizons@gmail.com>
2022-11-25 15:50:29 +0000

    Create election_data.py

---
+++ election_data.py
@@ -0,0 +1,18 @@
+city1 = {
+    "Cardiff West": {
+        'C': 11014,
+        'L': 17803,
+        'UKIP': 4923,
+        'LD': 2069
+    }
+}
+
+city2 = {
+
+}
+
+party_sym_lookup = {
+    "C": 'Conservative Party',
+    'L': "Labour Party",
+    'UKIP': 'UKIP'
+}
