68fe6f710e936b7738895cf75e0acf7c09e72dd1
saurabhsahugit <saurabh.newhorizons@gmail.com>
2022-11-27 22:20:12 +0000

    Create improved_election_data.py

---
+++ improved_election_data.py
@@ -0,0 +1,26 @@
+voting_data = {
+    'Cardiff West': {
+        'C': 11014,
+        'L': 17803,
+        'UKIP': 4923,
+        'LD': 2069
+    },
+    'Islington South & Finsbury': {
+        'L':  22547,
+        'C': 9389,
+        'LD': 4829,
+        'UKIP': 3375,
+        'G': 3371,
+        'IND': 309
+    }
+}
+
+party_sym_lookup = {
+    'C': 'Conservative Party',
+    'L': "Labour Party",
+    'UKIP': 'UKIP',
+    'LD': 'Liberal Democrats',
+    'G': 'Green Party',
+    'IND': 'Independent',
+    'SNP': 'SNP'
+}
